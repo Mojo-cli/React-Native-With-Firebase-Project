af1188e6b54e584f2c45fb0c81d773efd99a3b78
Adwait Gaikwad <68456350+Mojo-cli@users.noreply.github.com>
2020-10-14 10:38:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import AppNavigator from './Navigation/AppNavigator';
+
+export default function App(){
+  return <AppNavigator />;
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA7wpjbuLDAockX2STYcGETJOuK-dVU3vs",
+    authDomain: "ass1-740f5.firebaseapp.com",
+    databaseURL: "https://ass1-740f5.firebaseio.com",
+    projectId: "ass1-740f5",
+    storageBucket: "ass1-740f5.appspot.com",
+    messagingSenderId: "123888412887",
+    appId: "1:123888412887:web:2490f19f8c7383f350e892"
+  };
